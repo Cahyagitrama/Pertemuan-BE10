e13791527f8fe82daf210a75d76276dd75419a61
Cahya Ramadhan <129404199+Cahyagitrama@users.noreply.github.com>
2024-12-18 14:08:08 +0700

    Add files via upload

---
+++ Cahya Ramadhan-Pertemuan10/synchronous.js
@@ -0,0 +1,9 @@
+console.log("Rama Membuka Bowser Google Chrome.");
+
+console.log(`
+Rama download file selama 1 jam.
+  Downloading 1 Hour ...
+  Download Complete.
+`);
+
+console.log("Rama membuka Youtube.");
